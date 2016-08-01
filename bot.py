8e2f71016fc79e1c0a18ba67c49974f1b239849a
Floretta <Moonlington@users.noreply.github.com>
2016-08-01 12:39:35 +0200

    Almost forgot the checks, how dumb am I.

--- bot.py
+++ bot.py
@@ -103,6 +103,25 @@ def load_credentials():
     with open('credentials.json') as f:
 		return json.load(f)
 
+
+@bot.command()
+async def load(extension_name : str):
+    """Loads an extension."""
+    try:
+        bot.load_extension(extension_name)
+    except (AttributeError, ImportError) as e:
+        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        return
+    await bot.say("{} loaded.".format(extension_name))
+
+
+@bot.command()
+async def unload(extension_name : str):
+    """Unloads an extension."""
+    bot.unload_extension(extension_name)
+	await bot.say("{} unloaded.".format(extension_name))
+
+
 if __name__ == '__main__':
 	credentials = load_credentials()
     bot.client_id = credentials['client_id']
---
+++ cogs/utils/checks.py
@@ -0,0 +1,41 @@
+from discord.ext import commands
+import discord.utils
+
+def is_owner_check(message):
+    return message.author.id == '139386544275324928'
+
+def is_owner():
+    return commands.check(lambda ctx: is_owner_check(ctx.message))
+
+def check_permissions(ctx, perms):
+    msg = ctx.message
+    if is_owner_check(msg):
+        return True
+
+    ch = msg.channel
+    author = msg.author
+    resolved = ch.permissions_for(author)
+    return all(getattr(resolved, name, None) == value for name, value in perms.items())
+
+def role_or_permissions(ctx, check, **perms):
+    if check_permissions(ctx, perms):
+        return True
+
+    ch = ctx.message.channel
+    author = ctx.message.author
+    if ch.is_private:
+        return False # can't have roles in PMs
+
+    role = discord.utils.find(check, author.roles)
+    return role is not None
+
+def mod_or_permissions(**perms):
+    def predicate(ctx):
+        return role_or_permissions(ctx, lambda r: r.name in ('Luna Mod', 'Luna Admin'), **perms)
+
+    return commands.check(predicate)
+
+def admin_or_permissions(**perms):
+    def predicate(ctx):
+        return role_or_permissions(ctx, lambda r: r.name == 'Luna Admin', **perms)
+    return commands.check(predicate)
