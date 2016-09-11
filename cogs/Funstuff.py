f6a99fc4cbeb772d8dc6526e0ec3c60d51abdc51
Floretta <Moonlington@users.noreply.github.com>
2016-09-11 10:52:14 +0200

    Added glitch command

--- cogs/Funstuff.py
+++ cogs/Funstuff.py
@@ -3,9 +3,10 @@ from discord.ext import commands
 from cogs.utils import checks
 import os
 import random
+import copy
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from itertools import islice, cycle
+from itertools import islice, cycle, tee
 from textblob import TextBlob
 from io import BytesIO
 import re
@@ -1386,3 +1387,104 @@ class Funstuff:
             if r.status == 200:
                 js = await r.json()
                 await self.bot.say(js['file'])
+
+    @commands.command(pass_context=True)
+    async def glitch(self, ctx, imagelink: str, amount: int=None, seed: int=None, iterations: int=None):
+        def pairwise(iterable):
+            """Awesome function from the itertools cookbook
+            https://docs.python.org/2/library/itertools.html
+            s -> (s0,s1), (s1,s2), (s2, s3), ...
+            """
+            a, b = tee(iterable)
+            next(b, None)
+            return zip(a, b)
+
+
+        class Glitchjpeg(object):
+
+            def __init__(self, image_bytes, amount, seed, iterations):
+                self.bytes = image_bytes
+                self.new_bytes = None
+                try:
+                    self.header_length = self.get_header_length()
+                except ValueError as e:
+                    raise click.BadParameter(message=e.message)
+
+                self.parameters = {
+                    'amount': amount,
+                    'seed': seed,
+                    'iterations': iterations
+                }
+
+                self.glitch_bytes()
+
+            def get_header_length(self):
+                """Get the length of the header by searching sequential 0xFF 0xDA
+                values. These values mark the end of a Jpeg header. We add two to give
+                us a little leeway. We don't want to mess with the header.
+                """
+
+                for i, pair in enumerate(pairwise(self.bytes)):
+                    if pair[0] == 255 and pair[1] == 218:
+                        result = i + 2
+                        return result
+
+                raise ValueError('Not a valid jpg!')
+
+            def glitch_bytes(self):
+                """Glitch the image bytes, after the header based on the parameters.
+                'Amount' is the hex value that will be written into the file. 'Seed'
+                tweaks the index where the value will be inserted, rather than just a
+                simple division by iterations. 'Iterations' should be self explanatory
+                """
+
+                amount = self.parameters['amount'] / 100
+                seed = self.parameters['seed'] / 100
+                iterations = self.parameters['iterations']
+
+                # work with a copy of the original bytes. We might need the original
+                # bytes around if we glitch it so much we break the file.
+                new_bytes = copy.copy(self.bytes)
+                new_bytes = bytearray(new_bytes)
+
+                for i in (range(iterations)):
+                    max_index = len(self.bytes) - self.header_length - 4
+
+                    # The following operations determine where we'll overwrite a value
+                    # Illustrate by example
+
+                    # 36 = (600 / 50) * 3
+                    px_min = int((max_index / iterations) * i)
+
+                    # 48 = (600 / 50) * 3 + 1
+                    px_max = int((max_index / iterations) * (i + 1))
+
+                    # 12 = 48 - 36
+                    delta = (px_max - px_min)  # * 0.8
+
+                    # 36 + (12 * 0.8)
+                    px_i = int(px_min + (delta * seed))
+
+                    # If the index to be changed is beyond bytearray length file set
+                    # it to the max index
+                    if (px_i > max_index):
+                        px_i = max_index
+
+                    byte_index = self.header_length + px_i
+                    new_bytes[byte_index] = int(amount * 256)
+
+                self.new_bytes = new_bytes
+        amount = random.randint(1,50) if amount is None else amount
+        seed = random.randint(1,99) if seed is None else seed
+        iterations = random.randint(1,500) if iterations is None else iterations
+
+        async with aiohttp.get(imagelink) as r:
+            data = await r.read()
+            if not imagelink.endswith(".jpg"):
+                stream = BytesIO(data)
+                im = Image.open(stream)
+                nstream = BytesIO()
+                im.save(nstream, format="JPEG")
+                data = nstream.getvalue()
+            newbytes = BytesIO(Glitchjpeg(data, amount, seed, iterations).new_bytes)
+            await self.bot.upload(newbytes, filename="Glitch.jpg", content="**Amount:** {}\n**Seed:** {}\n**Iterations:** {}".format(amount, seed, iterations))
